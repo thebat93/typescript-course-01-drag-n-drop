d1e29edc3d0cccfbf9ce647fc4bac2f4e9772670
thebat93 <ais_93@mail.ru>
2020-06-27 13:58:40 +0300

    Рендеринг формы

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-// Code goes here!
+class ProjectInput {
+  // Шаблон формы
+  templateElement: HTMLTemplateElement;
+
+  // Рутовый элемент приложения
+  hostElement: HTMLDivElement;
+
+  // Форма
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedHTML = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedHTML.firstElementChild as HTMLFormElement;
+
+    this.attach();
+  }
+
+  // Метод для рендеринга формы в рутовый элемент
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projInput = new ProjectInput();
