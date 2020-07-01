e121bbb4c03f4052ea2d098cbc8ca7d7732d765f
thebat93 <ais_93@mail.ru>
2020-07-01 22:54:30 +0300

    Добавление стейта приложения

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,52 @@
+// Стейт
+class ProjectState {
+  // Список подписчиков
+  private listeners: any[] = [];
+
+  // Стейт: список сохраненных проектов
+  private projects: any[] = [];
+
+  // Поле, хранящее инстанс класса (паттерн синглтон)
+  private static instance: ProjectState;
+
+  // Создание инстанса разрешено только с помощью getInstance()
+  private constructor() {}
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  // Метод для добавления подписчика
+  addListener(listenerFn: Function) {
+    this.listeners.push(listenerFn);
+  }
+
+  // Метод для добавления проекта в стейт
+  addProject(title: string, description: string, numOfPeople: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title,
+      description,
+      people: numOfPeople,
+    };
+
+    this.projects.push(newProject);
+
+    // Уведомляем подписчиков об изменении стейта
+    for (const listenerFn of this.listeners) {
+      // Передаем в качестве аргумента копию стейта
+      listenerFn(this.projects.slice());
+    }
+  }
+}
+
+const projectState = ProjectState.getInstance();
+
 // Валидация
 interface Validatable {
   value: string | number;
@@ -76,12 +125,17 @@ class ProjectList {
   // Секция со списком
   element: HTMLElement;
 
+  // Список проектов, которые требуется отрендерить
+  assignedProjects: any[];
+
   constructor(private type: "active" | "finished") {
     this.templateElement = document.getElementById(
       "project-list"
     )! as HTMLTemplateElement;
 
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    this.assignedProjects = [];
 
     const importedHTML = document.importNode(
       this.templateElement.content,
@@ -90,8 +144,27 @@ class ProjectList {
 
     this.element = importedHTML.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
+
+    // Добавление подписчика: рендер нового стейта при его изменении
+    projectState.addListener((projects: any[]) => {
+      this.assignedProjects = projects;
+      this.renderProjects();
+    });
+
     this.attach();
     this.renderContent();
+  }
+
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement;
+
+    for (const item of this.assignedProjects) {
+      const listItem = document.createElement("li");
+      listItem.textContent = item.title;
+      listEl.appendChild(listItem);
+    }
   }
 
   private renderContent() {
@@ -203,7 +276,7 @@ class ProjectInput {
     if (Array.isArray(userInput)) {
       const [title, description, people] = userInput;
 
-      console.log({ title, description, people });
+      projectState.addProject(title, description, people);
 
       this.clearInputs();
     }
